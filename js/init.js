34a83efbcee4227e038f970b24182868b49f025d
Ana Lizbeth Martínez <82017492+almfic43@users.noreply.github.com>
2021-04-06 03:38:33 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDdapj3H3Dd2w9DHpLHwbzwpwSwU7rItpg",
-    authDomain: "chatrickfb.firebaseapp.com",
-    projectId: "chatrickfb",
-    storageBucket: "chatrickfb.appspot.com",
-    messagingSenderId: "815561540833",
-    appId: "1:815561540833:web:2cf0f52c625e8e281723a0",
-    measurementId: "G-5W02XQR53J"
+    apiKey: "AIzaSyBDLCqEdrixNIIU-S1TuxjDMKlonvEqb_A",
+    authDomain: "autentifica43almf-6088a.firebaseapp.com",
+    projectId: "autentifica43almf-6088a",
+    storageBucket: "autentifica43almf-6088a.appspot.com",
+    messagingSenderId: "32135909059",
+    appId: "1:32135909059:web:23cd9f0e91c9547c7b1c76",
+    measurementId: "G-QVKVDVERF5"
     });
