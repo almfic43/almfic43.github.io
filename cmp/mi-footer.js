cc3ea3623945e6fd00013a48f136997afdefd58d
Ana Lizbeth Martínez <82017492+almfic43@users.noreply.github.com>
2021-04-14 00:36:20 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,8 @@
 class MiFooter extends HTMLElement {
   connectedCallback(){
     this.innerHTML = /* html */
-  `Copyright &copy; 2020 Ana Lizbeth Martínez Francisco.`;
+  `Copyright &copy; 2021
+Ana Lizbeth Martínez Francisco.`;
   }
 }
 customElements.define("mi-footer", MiFooter);
